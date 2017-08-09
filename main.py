89e4f073f07edfa80fc16b34cf3b680a8e3c5f82
carlton566 <mascarenhascarlton56789@gmail.com>
2017-08-09 19:03:27 +0530

    big

--- main.py
+++ main.py
@@ -1,7 +1,47 @@
 #!usr/bin/python
+import wsgiref.handlers 
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp \
+   import template
+   
+   
+class product(db.Model):
+     firstname = db.StringProperty(
+	                 required=True)
+	 when = db.DateTimeProperty(
+	              auto_now_add=True)
+     phone = db.IntegerProperty(
+	                required=True)
 
-import cgi
-form = cgi.FieldStorage()
-with open ('fileToWrite.txt','w') as fileOutput:
-    fileOutput.write(form.getValue('firstname'))
-    fileOutput.write(form.getValue('lastname'))
+class MyHandler(webapp.RequestHandler):
+   def get(self):
+     products = db.GQlQuery(
+	   'SELECT * FROM Product '
+	   'ORDER BY when DESC')
+	   values = {
+	     'products': products
+	   }
+    self.response.out.write(
+	 template.render('main.html', 
+	                 values))
+   def post(self):
+     product = Product(
+	   firstname=self.request.get(
+	   'firstname'),
+	   phone=self.request.get(
+	   'phone'))
+	   
+	   product.put()
+     self.redirect('/')
+
+   
+   
+   
+   def main():
+app = webapp.WSGIApplication([
+  (r'.*',MyHandler)], debug=True)
+   wsgiref.handlers.CGIHandler().run(app)
+
+if __name__ == "__main__":
+    main()   
