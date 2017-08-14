2472adde0fa8ce7adba94bf43ceb5eacd9f559f1
carlton566 <mascarenhascarlton56789@gmail.com>
2017-08-15 00:03:27 +0530

    last one i guess

--- main.py
+++ main.py
@@ -2,46 +2,22 @@
 import wsgiref.handlers 
 from google.appengine.ext import db
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp \
-   import template
-   
+import webapp2
+import template
+import os
+import urllib   
    
 class product(db.Model):
-    firstname = db.StringProperty(
-	                 required=True)
-when = db.DateTimeProperty(
-	              auto_now_add=True)
-    phone = db.IntegerProperty(
-	                required=True)
-
-class MyHandler(webapp.RequestHandler):
+     productm = db.StringProperty(required=True)
+     description = db.StringProperty(required=True)
+     when = db.DateTimeProperty(auto_now_add=True)
+class MyHandler(webapp2.RequestHandler):
    def get(self):
-     products = db.GQlQuery(
-	   'SELECT * FROM Product '
-	   'ORDER BY when DESC')
-	 values = {
-	     'products': products
-	   }
-    self.response.out.write(
-	 template.render('main.html', 
-	                 values))
+     products = db.GQlQuery('SELECT * FROM Product ORDER BY when DESC')
+     values = {'products': products}
+     self.response.out.write(template.render('main.html', values))
    def post(self):
-     product = Product(
-	   firstname=self.request.get(
-	   'firstname'),
-	   phone=self.request.get(
-	   'phone'))
-	   
-	 product.put()
+     product = Product(productm=self.request.get('productm'), description=self.request.get( 'description'))
+     product.put()
      self.redirect('/')
-
-   
-   
-   
-   def main():
-app = webapp.WSGIApplication([
-  (r'.*',MyHandler)], debug=True)
-   wsgiref.handlers.CGIHandler().run(app)
-
-if __name__ == "__main__":
-    main()   
+app = webapp2.WSGIApplication([('/', MyHandler)], debug=True)  
