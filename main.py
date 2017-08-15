8cf48e922c7fa1c17ff1ac7d2804a1613a96d134
carlton566 <mascarenhascarlton56789@gmail.com>
2017-08-15 16:25:51 +0530

    change of plan

--- main.py
+++ main.py
@@ -1,22 +1,61 @@
 #!usr/bin/python
-import wsgiref.handlers 
-from google.appengine.ext import db
-from google.appengine.ext import webapp
-import template
-import os
-import urllib   
+import webapp2
+from google.appengine.ext import ndb
    
-class product(db.Model):
-     productm = db.StringProperty(required=True)
-     description = db.StringProperty(required=True)
-     when = db.DateTimeProperty(auto_now_add=True)
+   
+html = """ 
+             <html>
+            <body>
+			<h1>Enter Product Description</h1>
+            <form action = "/confirm" method = "POST">
+	     
+ Product:<input type = "text" name = "productm" id="productm" />
+ <br>
+ Description:<textarea rows="4" cols="50" id="description" name="description"></textarea>
+<input type = "submit"  value ="Save">
+</form>
+    <h1>Search Product</h1>
+	        <form> action="/search" method="POST">
+Product:<input type="text" name="productm">
+<br>
+<input type="submit" value="Search">
+</form>
+            </body>
+            </html>   
+ """  
+   
+   
+class Product(ndb.Model):
+     producte = ndb.StringProperty(indexed=True)
+     descriptions = ndb.TextProperty(indexed=True)
+     when = ndb.DateTimeProperty(auto_now_add=True)
+	 
+	 
+	 
 class MyHandler(webapp2.RequestHandler):
-   def get(self):
-     products = db.GQlQuery('SELECT * FROM Product ORDER BY when DESC')
-     values = {'products': products}
-     self.response.out.write(template.render('main.html', values))
+    def get(self):
+        self.response.out.write(html)
+		
+		
+		
+		
+class MainHandler(webapp2.RequestHandler):
    def post(self):
-     product = Product(productm=self.request.get('productm'), description=self.request.get( 'description'))
-     product.put()
-     self.redirect('/')
-app = webapp2.WSGIApplication([('/', MyHandler)], debug=True)  
+     product = self.request.get('productm')
+     description = self.request.get('description')
+     products = Product()
+     products.producte=product
+     products.descriptions=description
+     products.put()
+     self.response.out.write('Details enetered into the datastore are')
+
+class MainHandlers(webapp2.RequestHandler):
+     def post(self):
+            product = self.request.get('productm')
+	    products = Product.query()
+	    searchquery = products.filter(Product.producte==product)
+            for i in searchquery
+                self.response.out.write('<b>The product name is %s</b>',%i.producte)
+	 
+app = webapp2.WSGIApplication([('/', MyHandler),('/confirm', MainHandler),('/search', MainHandlers)], 
+ debug=True)
